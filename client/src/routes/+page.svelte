57019e5914c761f5fd46e3e5801af01d93e7238b
azedeveloper <albin.business@yahoo.com>
2025-02-05 13:15:59 +0100

    debloating

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
-    import { onMount } from "svelte";
     import PostList from "../components/PostList.svelte";
-
-    function navigate(path) {
-        window.location = path;
-    }
-
 </script>
 
 <div class="flex items-center justify-center min-h-screen bg-neutral-950">
